0934b9d08d261d9a9a5af76ce385ef3def4b7bb6
eg2525 <149653970+eg2525@users.noreply.github.com>
2024-11-05 15:10:21 +0900

    Add files via upload

--- home.py
+++ home.py
@@ -1,6 +1,8 @@
 import streamlit as st
-import excel_to_R4_kaneko
 import template
+import excel_to_R4_kaneko
+import excel_to_R4
+import excel_to_R4_bumon
 
 # 初期状態では何も表示しないようにセッション状態を設定
 if 'current_app' not in st.session_state:
@@ -10,11 +12,17 @@ st.title('EG_現金出納帳取込アプリ')
 
 
 # ボタンが押されたときに実行する関数
+def show_app0():
+    st.session_state['current_app'] = 'app0'
+
 def show_app1():
     st.session_state['current_app'] = 'app1'
 
-def show_app0():
-    st.session_state['current_app'] = 'app0'
+def show_app2():
+    st.session_state['current_app'] = 'app2'
+
+def show_app3():
+    st.session_state['current_app'] = 'app3'
 
 # サイドバーでアプリ選択用のボタンを表示
 with st.sidebar:
@@ -22,9 +30,17 @@ with st.sidebar:
         show_app0()
     if st.button('金子宝泉堂', key='app1'):
         show_app1()    
+    if st.button('部門なし', key='app2'):
+        show_app1()    
+    if st.button('部門あり', key='app3'):
+        show_app1()    
 
 # 選択されたアプリを表示
 if st.session_state['current_app'] == 'app0':
     template.app0()
 elif st.session_state['current_app'] == 'app1':
     excel_to_R4_kaneko.app1()
+elif st.session_state['current_app'] == 'app1':
+    excel_to_R4.app2()
+elif st.session_state['current_app'] == 'app1':
+    excel_to_R4_bumon.app3()

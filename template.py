24c95c5aff2d8fcdfc07f6026e82fbe8f6852821
eg2525 <149653970+eg2525@users.noreply.github.com>
2024-11-05 14:44:04 +0900

    Add files via upload

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ def app0():
     TEMPLATE_EXCEL_URLS = {
         "部門なし": "https://github.com/yourusername/yourrepo/raw/main/template1.xlsx",
         "部門あり": "https://github.com/yourusername/yourrepo/raw/main/template2.xlsx",
-        "その他": "https://github.com/eg2525/conversion_from_xlsx_to_r4/test.xlsx"
+        "その他": "https://github.com/eg2525/conversion_from_xlsx_to_r4/raw/main/test.xlsx"
     }
 
     # ページタイトル
